87790dc29a80a797f7b9db7a5bfb8ccabdab4796
BarakOren <barakoren5@gmail.com>
2022-11-15 21:01:12 +0300

    update

--- src/components/LogosSection/LogosSection.js
+++ src/components/LogosSection/LogosSection.js
@@ -1,134 +1,155 @@
 import styled, {keyframes} from "styled-components";
-import {ReactLogo} from '@styled-icons/fa-brands/ReactLogo'
 import { Parallax } from "react-scroll-parallax";
 import {Redux} from '@styled-icons/boxicons-logos/Redux'
+import {ReactLogo} from '@styled-icons/fa-brands/ReactLogo'
+import {Firebase} from '@styled-icons/boxicons-logos/Firebase'
+import {Nuxtdotjs} from '@styled-icons/simple-icons/Nuxtdotjs'
+import {Tailwindcss} from '@styled-icons/simple-icons/Tailwindcss'
+import {Webflow} from '@styled-icons/simple-icons/Webflow'
+import {Styledcomponents} from '@styled-icons/simple-icons/Styledcomponents'
+import {Nextdotjs} from '@styled-icons/simple-icons/Nextdotjs'
+import {Figma} from '@styled-icons/fa-brands/Figma'
 
-const Star = styled.span`
+
+const Label = styled.p`
     position: absolute;
-    top: ${p => `${p.top}%`};
-    left: ${p => `${p.left}%`};
-    width: ${p => `${p.size}px`};
-    height: ${p => `${p.size}px`};
-    border-radius: 50%;
-    background-color: white;
-    box-shadow: 0 0 5px white;
-    filter: ${p => `blur(${p.blur}px)`};
-    transition: .2s all;
-`
-
-
-const Pop = color => keyframes`
-      0% {
-          filter: ${p => `drop-shadow(0 0 0px #${p.color})`}
-      }
-      100% {
-        filter: ${p => `drop-shadow(0 0 50px #${p.color})`}
-    }
+    font-size: ${p => `${p.fontSize}px`}; 
+    top: ${p => `${p.top}px`};
+    margin: 0;
+    left: -100%;
+    color: white;
+    opacity: 0;
+    transition: 0.2s all;
+    color: ${p => `#${p.color}`};
+    text-shadow: ${p => `0 0 5px #${p.color}`};
+    // filter: blur(2px);
 `
 
 const React = styled(ReactLogo)`
     color: ${p => `#${p.color}`};
-    /* filter: ${p => `drop-shadow(3px 3px 4px #${p.color}55)`} ; */
+    filter: ${p => `drop-shadow(0px 0px 30px #${p.color}50)`} ; 
     transition: .2s all;
     position: relative;
     z-index: 5;
     &:hover {
-    /* filter: ${p => `drop-shadow(3px 3px 4px #${p.color})`} ; */
+    filter: ${p => `drop-shadow(0px 0px 30px #${p.color}80)`} ; 
     transform: scale(1.1);
-    animation: ${p => Pop(p.color)} 1s linear;
+    
     }
-    &:hover ~ ${Star} {
-        background-color: #61DBFB;
-        /* filter: drop-shadow(3px 3px 4px #61DBFB); */
+    &:hover ~ ${Label} {
+        left: 50%;
+        transform: translateX(-50%);
+        filter: ${p => `drop-shadow(3px 3px 4px ${p.color}) `};
+        opacity: 1;
     }
 `
 
 const ReduxLogo = styled(Redux)`
     color: ${p => `#${p.color}`};
-    filter: ${p => `drop-shadow(3px 3px 4px #${p.color}55)`} ;
+    filter: ${p => `drop-shadow(0px 0px 30px #${p.color}50)`} ;
     transition: .2s all;
     position: relative;
     z-index: 5;
     &:hover {
-    filter: ${p => `drop-shadow(3px 3px 4px #${p.color})`} ;
+    filter: ${p => `drop-shadow(0px 0px 30px #${p.color})`} ;
     transform: scale(1.1);
     }
-    &:hover ~ ${Star} {
-        background-color: #764abc;
-        filter: drop-shadow(3px 3px 4px #764abc);
+    &:hover ~ ${Label} {
+        left: 50%;
+        transform: translateX(-50%);
+        filter: ${p => `drop-shadow(3px 3px 4px ${p.color}) `};
+        opacity: 1;
     }
 `
 
-const Container = styled.div`
-    width: 100vw;
-    height: 150vh;
-    margin: 100px 0;
+
+const StyledComponentsLogo = styled(Styledcomponents)`
+    color: ${p => `#${p.color}`};
+    filter: ${p => `drop-shadow(0px 0px 30px #${p.color}20)`} ;
+    transition: .2s all;
     position: relative;
+    z-index: 5;
+    font-size: 10vw;
+    &:hover {
+    filter: ${p => `drop-shadow(0px 0px 30px #${p.color}80)`} ;
+    transform: scale(1.1);
+    }
+    &:hover ~ ${Label} {
+        left: 50%;
+        transform: translateX(-50%);
+        filter: ${p => `drop-shadow(3px 3px 4px ${p.color}) `};
+        opacity: 1;
+    }
+`
+
+
+const FigmaLogo = styled(Figma)`
+    color: ${p => `#${p.color}`};
+    filter: ${p => `drop-shadow(0px 0px 30px #${p.color}20)`} ;
+    transition: .2s all;
+    position: relative;
+    z-index: 5;
+    &:hover {
+    filter: ${p => `drop-shadow(0px 0px 30px #${p.color}80)`} ;
+    transform: scale(1.1);
+    }
+    &:hover ~ ${Label} {
+        left: 50%;
+        transform: translateX(-50%);
+        filter: ${p => `drop-shadow(3px 3px 4px ${p.color}) `};
+        opacity: 1;
+    }
 `
 
 const FlexBox = styled(Parallax)`
-    width: 100%;
-    height: 100%;
+    width: 100vw;
+    height: 100vh;
+    margin: 100px 0;
     display: flex;
     align-items: center;
     justify-content: space-evenly;
-    z-index: 5;
-
+    flex-wrap: wrap;
 `
 
 const IconContainer = styled.div`
-    width: 30%;
-    z-index: 2;
-    height: 100%;
     display: flex;
     align-items: center;
     justify-content: center;
-`
+    position: relative;
+    height: 50%;
+    width: ${p => p.width};
+    // margin: 50px 0;
+    `
 
-const ParallaxStars = styled(Parallax)`
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    left: 0;
-    top: -25%;
-    z-index: 0;
-
-`
 
 
 const LogosSection = () => {
-    const starsCount = []
-    for(let i = 0; i < 100; i++){
-        starsCount.push(i)    
-    }
-
-    const returnRandom = (min, max) => {
-        return Math.floor(Math.random() * (max - min + 1) + min)
-    }
     
-    return <Container>
-        <FlexBox
+    return <FlexBox
         translateY={['-100px', '100px']}
         scale={[1, 1.2]}
         >
-        <React color={'61DBFB'} size={'200'}></React>
-        <ReduxLogo color={'764abc'} size={'200'}></ReduxLogo>
-        <React color={'61DBFB'} size={'200'}></React>
-        {/* <ParallaxStars 
-        translateY={['-200px', '500px']}
-        >  */}
-        {/* {starsCount.map((star, i) => {
-                return <Star 
-                left={returnRandom(1, 100)} 
-                top={returnRandom(1, 100)}
-                size={returnRandom(1, 7)}
-                blur={returnRandom(0.2, 3)}
-                />
-        })} */}
-            {/* </ParallaxStars> */}
+        <IconContainer width={"30%"}>
+        <React color={'61DBFB'} size={'200'}  /> 
+        <Label color={'61DBFB'} fontSize={'60'} top={'-30'}>React</Label>
+        </IconContainer>
+
+        <IconContainer width={"30%"}>
+        <ReduxLogo color={'764abc'} size={'200'}  /> 
+        <Label color={'764abc'} fontSize={'60'} top={'-30'}>Redux</Label>
+        </IconContainer>
+        
+        <IconContainer width={"30%"}>
+        <StyledComponentsLogo color={'ffd561'}   />
+        <Label color={'ffd561'} fontSize={'40'} top={'-30'}>Styled-Components</Label>
+        </IconContainer>
+
+        <IconContainer>
+        <FigmaLogo color={'ffd561'} size={'80'} />
+        <Label color={'ffd561'} fontSize={'20'} top={'40'}>Styled-Components</Label>
+        </IconContainer>
+
         </FlexBox>
-       
-    </Container>
 }
 
 export default LogosSection;
